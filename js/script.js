444994e562b5722e3e25a23a05f577e0dcccd880
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-24 18:25:09 +0700

    menambahkan efek parallax

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // event pada saat link diklik
 $('.page-scroll').on('click', function(e){
     // ambil isi href
-    let href = $(this).attr('href');
+    const href = $(this).attr('href');
     // tangkap elemen yang diklik
-    let elemenHref = $(href);
+    const elemenHref = $(href);
     
     // pindahkan scroll
     $('html, body').animate({
@@ -11,4 +11,43 @@ $('.page-scroll').on('click', function(e){
     }, 1250, 'swing');
 
     e.preventDefault();
+});
+
+
+// parallax
+// about
+$(window).on('load', function(){
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+$(window).scroll(function(){
+    const windowScroll = $(this).scrollTop();
+
+    // jumbotron
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px,' + windowScroll/4 + '%)'
+    });
+
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px,' + windowScroll/6 + '%)'
+    });
+
+    $('.jumbotron h2').css({
+        'transform' : 'translate(0px,' + windowScroll/1.3 + '%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px,' + windowScroll/1.3 + '%)'
+    });
+
+
+    // portfolio
+    if(windowScroll > $('.portfolio').offset().top - 250){
+           $('.portfolio .thumbnail').each(function (i) {
+                setTimeout(function(){
+                    $('.portfolio .thumbnail').eq(i).addClass('muncul');
+                }, 300  * i);       
+           });
+    }
 });
